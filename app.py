e967981d7701fc2358f5ae1b8f2d6fc0cba0928e
Sohail <sohailshamas@yahoo.com>
2020-06-16 07:32:17 +0000

    Setup environment variables & connection to MONGODB

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+
+if os.path.exists("env.py"):
+    import env 
 
 
 app = Flask(__name__)
 
+"""MongoDB - setting env variables"""
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'hello world'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template('recipes.html', recipe=mongo.db.recipe.find())
 
 
 if __name__ == '__main__':
