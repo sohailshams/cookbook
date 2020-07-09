7d5348e76b72d546bdb1ea93f0ba2d32f5062bc0
Sohail <sohailshamas@yahoo.com>
2020-07-09 19:45:28 +0000

    Added js folder, script.js file & moved JS/Jquery code in here from base.html

---
+++ static/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    // code initializer for collapsible
+    $(".collapsible").collapsible();
+    // code initializer for select
+    $("select").material_select();
+    // code initializer for side navbar
+    $(".button-collapse").sideNav();
+    // code initializer for carousel(image slider)
+    $(".carousel").carousel();
+    setInterval(function() {
+        $(".carousel").carousel("next");
+    }, 3000);
+    // code initializer for modal
+    $(".modal").modal();
+    // code initializer for flash message
+    $(".alert.success").hide().delay(1000).fadeIn(200).delay(4000).fadeOut(200);
+});
+// Added underline code to show feedback message of required in the select
+//source stack overflow
+$("select[required]").css({
+    display: "block",
+    height: 0,
+    padding: 0,
+    width: 0,
+    position: "absolute",
+});
+$(".datepicker").pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: "Today",
+    clear: "Clear",
+    close: "Ok",
+    closeOnSelect: false, // Close upon selecting a date,
+});
+// Added underline code to make date picker field required
+// source stack overflow
+$(".datepicker").prop("readonly", false);
